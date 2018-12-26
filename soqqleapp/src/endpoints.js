15283079e63ec4b06f4afebb81cfc988b295f0bb
Khushhal Gupta <khushalgupta.98@gmail.com>
2018-12-27 01:06:18 +0530

    UserTasks: Updating endpoint to stg endpoint

--- soqqleapp/src/endpoints.js
+++ soqqleapp/src/endpoints.js
@@ -1,4 +1,4 @@
 export const STORIES_LIST_API = 'https://stgapi.soqqle.com/storiesGetAll';
 export const STORY_HAS_VIDEO_API = 'https://stgapi.soqqle.com/story/{}/has-video';
 export const AGENDA_LIST_API = 'https://stgapi.soqqle.com/taskgroupGetAll?page={}';
-export const USER_TASK_GROUP_LIST_API = 'http://localhost:3001/userTaskGroup?page={page}&type={type}';
+export const USER_TASK_GROUP_LIST_API = 'https://stgapi.soqqle.com/userTaskGroup?page={page}&type={type}';
