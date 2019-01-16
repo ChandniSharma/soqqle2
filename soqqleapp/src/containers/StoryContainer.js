0bb5fe19a7c7f4568852c7d3811b323fc97742d3
trucnd <trucnd@nexlesoft.com>
2019-01-16 10:03:28 +0700

    fix story containter

--- soqqleapp/src/containers/StoryContainer.js
+++ soqqleapp/src/containers/StoryContainer.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
 import StoryView from '../views/StoryView';
+import {isImmutable} from "immutable";
 
 export default connect(
     state => ({
         isReady: state.getIn(['session', 'isReady']),
-        user: state.getIn(['user', 'user']),
+        user: isImmutable(state.getIn(['user', 'user']))?state.getIn(['user', 'user']).toJS():state.getIn(['user', 'user']),
     })
 )(StoryView);
