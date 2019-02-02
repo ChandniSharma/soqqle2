c4b14eb6628628496920f0c7863fbff54b63ed37
Dachi Natsvlishvili <dachinat@gmail.com>
2019-02-02 21:39:41 +0400

    Update DeveloperMenu components

--- soqqleapp/src/components/DeveloperMenu.ios.js
+++ soqqleapp/src/components/DeveloperMenu.ios.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
-import * as snapshot from '../utils/snapshot';
+import {TouchableOpacity, ActionSheetIOS, StyleSheet} from 'react-native';
 
-import {
-  TouchableOpacity,
-  ActionSheetIOS,
-  StyleSheet
-} from 'react-native';
+import * as snapshot from '../utils/snapshot';
 
 /**
  * Simple developer menu, which allows e.g. to clear the app state.
@@ -16,52 +12,49 @@ class DeveloperMenu extends Component {
   static displayName = 'DeveloperMenu';
 
   showDeveloperMenu() {
-    const options = {
-      clearState: 0,
-      showLogin: 1,
-      cancel: 2
-    };
+      const options = {
+          clearState: 0,
+          showLogin: 1,
+          cancel: 2
+      };
 
-    const callback = async index => {
-      if (index === options.clearState) {
-        await snapshot.clearSnapshot();
-        console.warn('(╯°□°）╯︵ ┻━┻ \nState cleared, Cmd+R to reload the application now');
-      }
-    };
+      const callback = async index => {
+          if (index === options.clearState) {
+              await snapshot.clearSnapshot();
+              console.warn('(╯°□°）╯︵ ┻━┻ \nState cleared, Cmd+R to reload the application now');
+          }
+      };
 
-    ActionSheetIOS.showActionSheetWithOptions({
-      options: [
-        'Clear state',
-        'Cancel'
-      ],
-      cancelButtonIndex: options.cancel
-    }, callback);
+      ActionSheetIOS.showActionSheetWithOptions({
+          options: [
+              'Clear state',
+              'Cancel'
+          ],
+          cancelButtonIndex: options.cancel
+      }, callback);
   }
 
   render() {
-    if (!__DEV__) {
-      return null;
-    }
+      if (!__DEV__) {
+          return null;
+      }
 
-    return (
-      <TouchableOpacity
-        style={styles.circle}
-        onPress={this.showDeveloperMenu}
-        />
-    );
+      return (
+          <TouchableOpacity style={styles.circle} onPress={this.showDeveloperMenu} />
+      );
   }
 }
 
 const styles = StyleSheet.create({
-  circle: {
-    position: 'absolute',
-    bottom: 5,
-    right: 5,
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    backgroundColor: '#fff'
-  }
+    circle: {
+        position: 'absolute',
+        bottom: 5,
+        right: 5,
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: '#fff'
+    }
 });
 
 export default DeveloperMenu;
