88077dd845bdf33b1b79dc76318f10a59857a654
Dachi Natsvlishvili <dachinat@gmail.com>
2019-02-02 21:26:23 +0400

    Update src/navigator/*: Minor changes

--- soqqleapp/src/navigator/Navigator.js
+++ soqqleapp/src/navigator/Navigator.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import {createStackNavigator} from 'react-navigation';
-import LoginContainer from '../containers/LoginContainer';
-import ProfileContainer from '../containers/ProfileContainer';
-import CompanyProfileContainer from '../containers/CompanyContainer';
-import StoryContainer from '../containers/StoryContainer';
-import AgendaContainer from '../containers/AgendaContainer';
-import UserTaskGroupContainer from '../containers/UserTaskGroupContainer';
-import ChatContainer from '../containers/ChatContainer';
-import DashboardContainer from '../containers/DashboardContainer';
-import TaskContainer from "../containers/TaskContainer";
 
-import UsersList from '../views/UsersList';
-import UserDetailView from '../views/UserDetailView';
+import LoginContainer from './../containers/LoginContainer';
+import ProfileContainer from './../containers/ProfileContainer';
+import CompanyProfileContainer from './../containers/CompanyContainer';
+import StoryContainer from './../containers/StoryContainer';
+import AgendaContainer from './../containers/AgendaContainer';
+import UserTaskGroupContainer from './../containers/UserTaskGroupContainer';
+import ChatContainer from './../containers/ChatContainer';
+import DashboardContainer from './../containers/DashboardContainer';
+import TaskContainer from './../containers/TaskContainer';
+import UsersList from './../views/UsersList';
+import UserDetailView from './../views/UserDetailView';
 
 const AppNavigator = createStackNavigator({
-    Login: {screen: LoginContainer},
-    UserTaskGroup: {screen: UserTaskGroupContainer},
-    Profile: {screen: ProfileContainer},
-    Story: {screen: StoryContainer},
-    Task: {screen: TaskContainer},
-    CompanyProfile: {screen: CompanyProfileContainer},
-    Chat: {screen: ChatContainer},
-    Agenda: {screen: AgendaContainer},
-    Dashboard: {screen: DashboardContainer},
-    UsersList:{screen: UsersList},
-    UserDetailView:{screen:UserDetailView},
-  },
-  {
-    headerMode: 'none'
-  }
+        Login: {screen: LoginContainer},
+        UserTaskGroup: {screen: UserTaskGroupContainer},
+        Profile: {screen: ProfileContainer},
+        Story: {screen: StoryContainer},
+        Task: {screen: TaskContainer},
+        CompanyProfile: {screen: CompanyProfileContainer},
+        Chat: {screen: ChatContainer},
+        Agenda: {screen: AgendaContainer},
+        Dashboard: {screen: DashboardContainer},
+        UsersList:{screen: UsersList},
+        UserDetailView:{screen:UserDetailView},
+    }, {
+        headerMode: 'none'
+    }
 );
 
 export default AppNavigator;
