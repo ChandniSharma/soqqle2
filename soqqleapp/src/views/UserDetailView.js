6eb1efb4aa8c19f8fd3e4309b207670d039c608c
Dachi Natsvlishvili <dachinat@gmail.com>
2019-02-02 20:39:39 +0400

    Reformat UserDetailView

--- soqqleapp/src/views/UserDetailView.js
+++ soqqleapp/src/views/UserDetailView.js
@@ -1,75 +1,79 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     TouchableOpacity,
     Text, View, SafeAreaView
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'
-import CardSection from '../components/CardSection';
 import {
     Thumbnail
-} from "native-base";
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { NavigationActions } from 'react-navigation';
+} from 'native-base';
+import {isImmutable} from 'immutable';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {NavigationActions} from 'react-navigation';
+
 import styles from './../stylesheets/userDetailViewStyles';
 import ProfileView from './ProfileView';
-import * as UserActions from '../reducers/UserReducer';
-import * as actions from '../reducers/SparkReducer';
-import { isImmutable } from 'immutable';
+import * as UserActions from './../reducers/UserReducer';
+import * as actions from './../reducers/SparkReducer';
 
 class UserDetailView extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            dataUser: {},
-        };
+        this.state = {dataUser: {},};
     }
+
     componentDidMount() {
         let dictUserDetail = this.props.navigation.state.params.detailDict;
-        this.setState({ dataUser: dictUserDetail });
+        this.setState({dataUser: dictUserDetail});
     }
+
     handleBackAction() {
-        this.props.navigation.navigate({ routeName: 'UsersList' })
+        this.props.navigation.navigate({routeName: 'UsersList'});
     }
+
     render() {
-        var name = '', designation = '', imgUser, bio = '';
+        let name = '';
+        let designation = '';
+        let imgUser;
+        let bio = '';
+
         let dictUserDetail = this.state.dataUser.userDetails;
         if (dictUserDetail) {
+            // TODO: name is not used?
             if (dictUserDetail.profile.firstName) {
-                name = dictUserDetail.profile.firstName
+                name = dictUserDetail.profile.firstName;
             }
             if (dictUserDetail.profile.lastName) {
                 name = name + ' ' + dictUserDetail.profile.lastName;
             }
             imgUser = <Thumbnail
                 style={styles.imageUser}
-                source={{ uri: dictUserDetail.profile.pictureURL || `https://ui-avatars.com/api/?name=${dictUserDetail.profile.firstName}+${dictUserDetail.profile.lastName}` }} />
+                source={{uri: dictUserDetail.profile.pictureURL || `https://ui-avatars.com/api/?name=${dictUserDetail.profile.firstName}+${dictUserDetail.profile.lastName}`}}/>;
 
             if (dictUserDetail.profile.title) {
                 designation = dictUserDetail.profile.title;
             }
             if (dictUserDetail.profile.bio) {
-                bio = dictUserDetail.profile.bio
+                bio = dictUserDetail.profile.bio;
             }
         }
-        let sparks = {
-            tokensCount: 0
-        }
+
+        let sparks = {tokensCount: 0};
         if (dictUserDetail) {
             return (
                 <ProfileView
-                 taskGroupData={this.props.navigation.state.params.taskGroupData} 
-                 backToUserList={true}
-                 userActions={this.props.userActions} 
-                 sparks={sparks} 
-                 user={dictUserDetail}
-                 navigation={this.props.navigation}
-                 companies={this.props.companies}
-                 sparks={this.props.sparks}
-                 sparkActions={this.props.sparkActions} 
+                    taskGroupData={this.props.navigation.state.params.taskGroupData}
+                    backToUserList={true}
+                    userActions={this.props.userActions}
+                    sparks={sparks}
+                    user={dictUserDetail}
+                    navigation={this.props.navigation}
+                    companies={this.props.companies}
+                    sparks={this.props.sparks}
+                    sparkActions={this.props.sparkActions}
 
-                 />
-            )
+                />
+            );
         } else {
             return null;
         }
