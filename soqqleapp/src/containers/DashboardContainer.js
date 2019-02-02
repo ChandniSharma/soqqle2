6767a9c0e847a667edcdd49d4d51ecb60346be2c
Dachi Natsvlishvili <dachinat@gmail.com>
2019-02-02 22:01:00 +0400

    Container refactory: DashboardContainer.js

--- soqqleapp/src/containers/DashboardContainer.js
+++ soqqleapp/src/containers/DashboardContainer.js
@@ -2,15 +2,19 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { NavigationActions } from 'react-navigation';
 import { isImmutable } from 'immutable';
-import * as achievementActions from '../reducers/AchievementReducer';
-import * as sparkActions from '../reducers/SparkReducer';
-import DashboardView from '../views/DashboardView';
+
+import * as achievementActions from './../reducers/AchievementReducer';
+import * as sparkActions from './../reducers/SparkReducer';
+import DashboardView from './../views/DashboardView';
 
 export default connect(
     state => ({
-        user: isImmutable(state.getIn(['user', 'user'])) ? state.getIn(['user', 'user']).toJS() : state.getIn(['user', 'user']),
-        achievements: isImmutable(state.getIn(['achievement', 'details'])) ? state.getIn(['achievement', 'details']).toJS() : state.getIn(['achievement', 'details']),
-        sparks: isImmutable(state.getIn(['spark', 'details'])) ? state.getIn(['spark', 'details']).toJS() : state.getIn(['spark', 'details']),
+        user: isImmutable(state.getIn(['user', 'user'])) ? state.getIn(['user', 'user']).toJS() :
+            state.getIn(['user', 'user']),
+        achievements: isImmutable(state.getIn(['achievement', 'details'])) ?
+            state.getIn(['achievement', 'details']).toJS() : state.getIn(['achievement', 'details']),
+        sparks: isImmutable(state.getIn(['spark', 'details'])) ? state.getIn(['spark', 'details']).toJS() :
+            state.getIn(['spark', 'details']),
     }),
     dispatch => {
         return {
