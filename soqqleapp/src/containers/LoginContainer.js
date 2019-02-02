f4a6a82421a90111dcd2b3f3e2c0c230e11c1eca
Dachi Natsvlishvili <dachinat@gmail.com>
2019-02-02 22:02:09 +0400

    Container refactory: LoginContainer.js

--- soqqleapp/src/containers/LoginContainer.js
+++ soqqleapp/src/containers/LoginContainer.js
@@ -1,15 +1,18 @@
-import { connect } from 'react-redux';
-import {isImmutable} from 'immutable';
-import LoginView from '../views/LoginView';
+import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {NavigationActions} from 'react-navigation';
-import * as UserActions from '../reducers/UserReducer';
+import {isImmutable} from 'immutable';
+
+import * as UserActions from './../reducers/UserReducer';
+import LoginView from './../views/LoginView';
 
 export default connect(
     state => ({
         isReady: state.getIn(['session', 'isReady']),
-        user: isImmutable(state.getIn(['user', 'user']))?state.getIn(['user', 'user']).toJS():state.getIn(['user', 'user']),
-        checkEmailResult: isImmutable(state.getIn(['user', 'checkEmailResult']))?state.getIn(['user', 'checkEmailResult']).toJS():state.getIn(['user', 'checkEmailResult']),
+        user: isImmutable(state.getIn(['user', 'user'])) ? state.getIn(['user', 'user']).toJS() :
+            state.getIn(['user', 'user']),
+        checkEmailResult: isImmutable(state.getIn(['user', 'checkEmailResult'])) ?
+            state.getIn(['user', 'checkEmailResult']).toJS() : state.getIn(['user', 'checkEmailResult']),
         loginSuccess: state.getIn(['user', 'loginSuccess']),
         checkEmailSuccess: state.getIn(['user', 'checkEmailSuccess']),
         forgotpasswordSuccess: state.getIn(['user', 'forgotpasswordSuccess']),
