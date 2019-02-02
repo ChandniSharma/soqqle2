2d581666b0c36418090d664576a3435ad0cda3e7
Dachi Natsvlishvili <dachinat@gmail.com>
2019-02-02 21:35:44 +0400

    Update Card.js component

--- soqqleapp/src/components/Card.js
+++ soqqleapp/src/components/Card.js
@@ -1,34 +1,31 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const Card = (props) =>{
-
-    return (
-        <View style={styles.containerStyle}> 
-          {props.children}
-        </View>
-    )
-
-   
-}
-
-
-export default Card  ;
 const styles = {
-    containerStyle:{
+    containerStyle: {
         borderColor: 'black',
-       // borderTopColor: 'green',
+        // borderTopColor: 'green',
         borderTopWidth: 1,
-       // borderBottomColor: 'black',
+        // borderBottomColor: 'black',
         borderBottomWidth: 1,
         borderRadius: 2,
         shadowColor: 'black',
-        shadowOffset: {width:1, height:2},
+        shadowOffset: {width: 1, height: 2},
         shadowOpacity: 0.5,
-        
-       marginLeft: 5,
-       marginRight: 5,
-       marginTop: 100,
-       marginBottom: 10,
+
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 100,
+        marginBottom: 10,
     }
-}
+};
+
+const Card = props => {
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    );
+};
+
+export default Card;
