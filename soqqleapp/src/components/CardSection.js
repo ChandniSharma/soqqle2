575baae85bd47f83ffc16bf47d4aea4a6f5730d1
Dachi Natsvlishvili <dachinat@gmail.com>
2019-02-02 21:36:43 +0400

    Update CardSection.js component

--- soqqleapp/src/components/CardSection.js
+++ soqqleapp/src/components/CardSection.js
@@ -1,28 +1,25 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { Card } from './Card';
 
-const CardSection = (props) =>{
-
-    return (
-        <View style={[styles.viewContainer, props.style]}>
-          {props.children}
-        </View>
-    );
-
-}
-
-export default CardSection ;
+import {Card} from './Card';
 
 const styles = {
-    viewContainer:{
+    viewContainer: {
         borderBottomWidth: 0,
         backgroundColor: 'fff',
         padding: 5,
         justifyContent: 'flex-start',
         flexDirection: 'row',
         position: 'relative',
-        
     }
-    
-}
+};
+
+const CardSection = props => {
+    return (
+        <View style={[styles.viewContainer, props.style]}>
+            {props.children}
+        </View>
+    );
+};
+
+export default CardSection;
