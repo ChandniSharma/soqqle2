3a4a6df8e0e80932deb8aae5749eecd7988880ef
Dachi Natsvlishvili <dachinat@gmail.com>
2019-02-02 21:42:39 +0400

    Update ListItem component

--- soqqleapp/src/components/ListItem.js
+++ soqqleapp/src/components/ListItem.js
@@ -1,98 +1,83 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react';
 import {Text, TouchableWithoutFeedback, View, TouchableOpacity, Share, Image} from 'react-native';
-import {CardSection} from './common/CardSection';
 
+//import CardSection from './CardSection';
 
-class ListItem extends Component{
-    onPressRow(){
-        
-    }
+class ListItem extends Component {
+    onPressRow() {}
+    render() {
+        //const {nameUser, phone, dob, image, repeatValue} = this.props.employee;
+        //console.log("in List Item  Name ************", nameUser, "employeeeee---", this.props.employee);
 
- 
+        let imageIcon = 'https://bootdey.com/img/Content/avatar/avatar6.png';
 
-render(){
-    const {nameUser,phone, dob, image, repeatValue} = this.props.employee;
-
-     //console.log("in List Item  Name ************", nameUser, "employeeeee---", this.props.employee); 
-    let  imageIcon  = 'https://bootdey.com/img/Content/avatar/avatar6.png';
-       
-         
-    return(
-        <TouchableWithoutFeedback onPress={this.onPressRow.bind(this)}>
-        <View style={styles.viewOuter}>
-            <View style={styles.viewInner}>
-            
-            <View style={styles.mainView}>
-            <View>
-              <Image source={{ uri: imageIcon }} style={styles.imageStyle} />
-            </View>
-                
-                   
-                
+        return (
+            <TouchableWithoutFeedback onPress={this.onPressRow.bind(this)}>
+                <View style={styles.viewOuter}>
+                    <View style={styles.viewInner}>
+                        <View style={styles.mainView}>
+                            <View>
+                                <Image source={{uri: imageIcon}} style={styles.imageStyle}/>
+                            </View>
+                        </View>
+                    </View>
                 </View>
-
-             </View>            
-             </View>
-        </TouchableWithoutFeedback>
-        
-    );
-}
+            </TouchableWithoutFeedback>
+        );
+    }
 }
 
-
-const styles={
-    callImg:{
-        width:32,
-        height:32
+const styles = {
+    callImg: {
+        width: 32,
+        height: 32
     },
-    message:{
-        width:32,
-        height:32
+    message: {
+        width: 32,
+        height: 32
     },
-    share:{
-        width:32,
-        height:32
+    share: {
+        width: 32,
+        height: 32
     },
-    imageStyle:{
-        width: 70, height: 70, borderRadius:35, 
-    }, 
-    mainView:{
-     flexDirection: 'row',
-     alignItems:'center'
+    imageStyle: {
+        width: 70, height: 70, borderRadius: 35,
     },
-    titleStyle:{
+    mainView: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    titleStyle: {
         fontSize: 18,
         // paddingLeft: 5,
     },
-    leftView:{
-      marginLeft: 5,
+    leftView: {
+        marginLeft: 5,
     },
-    rightView:{
-     right: '2%',
-    //  backgroundColor:'pink',
-     position:'absolute', 
-     
-    
+    rightView: {
+        right: '2%',
+        //  backgroundColor:'pink',
+        position: 'absolute',
+
+
     },
-    viewOuter:{
+    viewOuter: {
 
         padding: 10,
-     },
-    viewInner:{
+    },
+    viewInner: {
         padding: 15,
         backgroundColor: '#f2f2f2',
-        borderBottomColor : 'gray',
+        borderBottomColor: 'gray',
         borderWidth: 0.8,
         // borderLeftWidth: 0.5,
         shadowColor: 'black',
-        shadowOffset: {width:1.0,height:2},
+        shadowOffset: {width: 1.0, height: 2},
         shadowOpacity: 0.5,
         shadowRadius: 1.5,
         elevation: 3,
 
     },
-}
+};
 
 export default ListItem;
-
- 
